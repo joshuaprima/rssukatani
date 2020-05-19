2d325af08c6b422c3cb6bcdef1a3df4c4309577b
FIchsan <fiandasyahdan@gmail.com>
2020-05-20 00:13:53 +0700

    Insert to Itemlog sudah bisa, tapi session (untuk id pegawai), status (masih string doang) dan deskripsi (masih string doang) perlu diperbaiki. Note: jangan lupa kolom tgl_perubahan di DB typenya ganti jadi 'timestamp'

--- Functions/addNewInventory.php
+++ Functions/addNewInventory.php
@@ -1,9 +1,11 @@
 <?php 
 require_once 'connection.php' ;
 include 'generateUUID.php' ;
-$con = getDB(); 
+$con = getDB();
+$con2 = getDB(); 
 
 if(isset($_POST['submit'])) {
+    $empID = "1";
     $uuid = gen_UUID();
     $item = $_POST['InvName'];
     $distributor = $_POST['InvDist'];
@@ -14,13 +16,21 @@ if(isset($_POST['submit'])) {
     $price = $_POST['InvPrice'];
     $blud = $_POST['InvBlud'];
     $customExp = $_POST['InvCustom'];
+    $deskripsi = "Example Description";
+    $status = "Added";
     $expiredDate = date('Y-m-d ', strtotime('+1825 days'));
 
     $query = 
     "INSERT INTO barang (idbarang, nama, distributor, quantity,satuan,deskripsi,harga_satuan,tanggal_blud,cust_expired_date, expired_date, types)
     VALUES('$uuid','$item','$distributor','$quantity','$satuan','$desc','$price','$blud','$customExp','$expiredDate','$types')";
-    
+
+    $query2 = 
+    "INSERT INTO itemlog (idpegawai,idbarang,jumlah,deskripsi,status)
+    VALUES('$empID','$uuid','$quantity','$deskripsi','$status')";
+      
     $sql = mysqli_query($con,$query);
+    
+    $sql2 = mysqli_query($con2,$query2);
     
     if ($sql) {
         echo "<script type=\"text/javascript\">
